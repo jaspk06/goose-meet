98e80efec8d220c2dcbb5f32c11f1deafda2f8a7
Jason Liu <jliu@pi.com>
2020-05-29 15:01:16 -0400

    added send messages function

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -36,6 +36,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-detail/member-messages/member-messages.component';
 
 
 export function tokenGetter() {
@@ -62,7 +64,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      MemberMessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -96,6 +99,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MemberDetailResolver,
       MemberEditResolver,
       MemberListResolver,
+      MessagesResolver,
       ListsResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -12,24 +12,35 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builer)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            builer.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
+            builder.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
 
             //one likee has many likers
             //on delete only delete the like and not all the other objects with it
-            builer.Entity<Like>()
+            builder.Entity<Like>()
             .HasOne(u => u.Likee)
             .WithMany(u => u.Likers)
             .HasForeignKey(u => u.LikeeId)
             .OnDelete(DeleteBehavior.Restrict);
 
             //one liker has many likees
-            builer.Entity<Like>()
+            builder.Entity<Like>()
             .HasOne(u => u.Liker)
             .WithMany(u => u.Likees)
             .HasForeignKey(u => u.LikerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(m => m.MessagesReceived)
             .OnDelete(DeleteBehavior.Restrict);
         }
     }
